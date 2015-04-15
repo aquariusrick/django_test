17dac079ba1731b5a017d451b43fe0bce9f40f8e
Rick Garlick <rick.garlick@yahoo.com>
2015-04-15 15:45:21 -0700

    Writing your first Django app, part 2

--- rick_test/settings.py
+++ rick_test/settings.py
@@ -93,12 +93,13 @@ TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 # PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 #
-# TEMPLATE_DIRS = (
+TEMPLATE_DIRS = (
 #     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
 #     # Always use forward slashes, even on Windows.
 #     # Don't forget to use absolute paths, not relative paths.
 #     os.path.join(PROJECT_ROOT, 'templates').replace('\\','/'),
-# )
+    os.path.join(BASE_DIR, 'templates'),
+)
 #
 # # List of callables that know how to import templates from various sources.
 # TEMPLATE_LOADERS = (
